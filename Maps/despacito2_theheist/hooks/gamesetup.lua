541c7220a175f8e39156c8eb3a638c68c6d20a38
Jon Hansen-Blum <masterjon902@gmail.com>
2023-04-01 04:09:12 -0500

    Reorg master branch

--- Maps/despacito2_theheist/hooks/gamesetup.lua
+++
@@ -1,5 +0,0 @@
-SkyLib:set_mod_key(himom) -- The Global Key you specified in the main.xml is now a Global variable, do not use quotes!
--- Final Init
-SkyLib:init_by_gamemode("codzm")
-SkyLib.CODZ.WeaponHelper:_setup_box_weapons()
---SkyLib.CODZ:LoadSounds()
