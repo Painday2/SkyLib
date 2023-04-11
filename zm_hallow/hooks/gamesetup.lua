f4b10dd537378cd9f117e783d8815ecdec141107
LulsCupcake <meowcats2121@gmail.com>
2023-04-10 23:44:48 -0600

    Added zm_hollow
    Might become a full map, who knows

---
+++ zm_hallow/hooks/gamesetup.lua
@@ -0,0 +1,5 @@
+SkyLib:set_mod_key(zm_testing) -- The Global Key you specified in the main.xml is now a Global variable, do not use quotes!
+-- Final Init
+SkyLib:init_by_gamemode("codzm")
+SkyLib.CODZ.WeaponHelper:_setup_box_weapons()
+--SkyLib.CODZ:LoadSounds()
